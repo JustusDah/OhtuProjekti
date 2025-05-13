2dd4bbbf86aa322367feec9e5de47e43c2ac5a64
ananpn <anttpw@gmail.com>
2025-05-13 15:29:33 +0300

    fixes

--- src/main/java/com/OhtuProjekti/Screens/VarausScreen.java
+++ src/main/java/com/OhtuProjekti/Screens/VarausScreen.java
@@ -1,6 +1,17 @@
 package com.OhtuProjekti.Screens;
 
+import com.OhtuProjekti.Classes.Mokki;
+import com.OhtuProjekti.Classes.Varaus;
+import com.OhtuProjekti.DBManager;
+import com.OhtuProjekti.Popups.InsertMokkiPopup;
+import com.OhtuProjekti.Popups.MokkiPopup;
 import com.OhtuProjekti.SceneManager;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+import java.util.List;
 
 public class VarausScreen extends SuperScreen {
 
@@ -18,7 +29,47 @@ public class VarausScreen extends SuperScreen {
         super.createScreenSuper("Varaukset");
 
 
-        //screen.setCenter(contentBox);
+        VBox contentBox = new VBox(10);
+        contentBox.setPadding(new Insets(20));
+
+        Text title = new Text("Valitse mökki nähdäksesi tiedot:");
+        contentBox.getChildren().add(title);
+
+        VBox buttonBox = new VBox(5);
+        buttonBox.setFillWidth(true);
+
+        List<Varaus> varausList = DBManager.getAllVaraukset();
+        for (Varaus varaus : varausList) {
+            Button button = new Button("VarausId: " + String.valueOf(varaus.varausID));
+            button.setMaxWidth(Double.MAX_VALUE);
+            button.setPrefWidth(200);
+
+            button.setOnAction(e -> {
+                VarausPopup varausPopup = new VarausPopup();
+                varausPopup.createPopup(varaus);
+                varausPopup.showPopup();
+            });
+
+            buttonBox.getChildren().add(button);
+        }
+
+        contentBox.getChildren().add(buttonBox);
+
+
+        screen.setCenter(contentBox);
+
+
+        Button insertVarausButton = new Button("Lisää mökki");
+        insertVarausButton.setOnAction(e -> {
+            InsertVarausPopup insertVarausPopup = new InsertVarausPopup();
+            insertVarausPopup.createPopup();
+            insertVarausPopup.showPopup();
+
+        });
+        bottomPane.getChildren().add(insertVarausButton);
+
+
+
 
 
 
